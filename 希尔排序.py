d25630cecbc86c0ee943d76f24e2a88b3d05dee0
Leon92am <38450584+Leon92am@users.noreply.github.com>
2019-03-04 18:03:42 +0800

    Update 希尔排序.py

--- 希尔排序.py
+++ 希尔排序.py
@@ -7,10 +7,9 @@ def InsertSORT(array1):
     while gap>1:
         gap=gap//2
         for i in range(gap,len(array1)):#从第一个增量位置对应的元素开始，一次进行比较
-            for j in range(i%gap,i,gap):#比较对象为该元素之间间隔为增量的所有元素
-                if array1[j]>array1[i]:
-                    array1[i],array1[j]=array1[j],array1[i]
-                    break
+            while(i>=gap and array1[i-gap]>array1[i]):
+                array1[i],array1[j]=array1[j],array1[i]
+                i-=gap
     return array1
 
 
